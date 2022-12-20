6d9a66b34fa8608cee9b87025e2bc1dd60661368
nahwasa <nahwasa@gmail.com>
2022-12-20 10:56:11 +0900

    init

---
+++ src/main/java/com/nahwasa/practice/tdd/tddbookpractice/TddBookPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.practice.tdd.tddbookpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBookPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddBookPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nahwasa/practice/tdd/tddbookpractice/TddBookPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.practice.tdd.tddbookpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBookPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
