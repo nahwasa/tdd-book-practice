6946eb13d343bfecf9d1063e1365558c3a27d93d
nahwasa <nahwasa@gmail.com>
2022-12-20 16:22:06 +0900

    Dollar의 side effect 해결

--- src/test/java/com/nahwasa/practice/tdd/tddbookpractice/MoneyTest.java
+++ src/test/java/com/nahwasa/practice/tdd/tddbookpractice/MoneyTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MoneyTest {
+class MoneyTest {
     @Test
-    @DisplayName("5달러를 2배하면 10달러여야 한다.")
+    @DisplayName("5달러를 2배하면 10달러, 3배를 하면 15달러여야 한다.")
     void dollar_multiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
